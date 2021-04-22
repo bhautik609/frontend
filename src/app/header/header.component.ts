6eae2ce078ca3942264f50d4f57ff0752e7c37d2
bhuatik609 <bhautikprajapati609@gmail.com>
2021-04-22 17:03:57 +0530

    done

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { Observable } from 'rxjs';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { map, shareReplay } from 'rxjs/operators';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
   showFiller=false;
   username;
-  constructor( private _router:Router) { }
+  constructor( private _router:Router,private breakpointObserver: BreakpointObserver) { }
 
   ngOnInit(): void {
     this.username=localStorage.getItem('username');
